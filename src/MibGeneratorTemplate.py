11de4312559f021f078969376608f642ea01adac
Hassan Pouralijan <pouralijan@gmail.com>
2019-07-01 06:55:41 +0430

    Update template file and config file.

--- src/MibGeneratorTemplate.py
+++ src/MibGeneratorTemplate.py
@@ -1,3 +1,32 @@
+snmpFileTemplate = """{sectionName} DEFINITIONS ::= BEGIN
+{imports}
+
+{moduleIdentity}
+
+{topLevelStructure}
+
+{scalars}
+
+{tables}
+
+{notification}
+
+END
+"""
+
+importTemplates = """IMPORTS {modules} FROM {section}"""
+
+moduleIdentityTemplate = """{moduleName} MODULE-IDENTITY
+    LAST-UPDATED "{lastUpdate}"
+    ORGANIZATION "{organization}"
+    CONTACT-INFO "{contactInfo}"
+    DESCRIPTION "{description}"
+    REVISION     "{revision}"
+    ::= {curlyBracketOpen} {parentObject} {objectID} {curlyBracketClose}
+"""
+
+objectIdentifierTemplate = """{objectName} OBJECT IDENTIFIER 
+                ::= {curlyBracketOpen} {parentObject} {objectID} {curlyBracketClose}"""
 
 scalarObjectTemplate = """{objectName} OBJECT-TYPE
     SYNTAX      {objectType}
